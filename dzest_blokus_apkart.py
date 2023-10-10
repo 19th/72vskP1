aa414ff066ee314e3134e3b71d897020c45df649
Ilya <karpov.ilja@gmail.com>
2023-10-10 22:27:57 +0300

    Create dzest_blokus_apkart.py

---
+++ dzest_blokus_apkart.py
@@ -0,0 +1,8 @@
+# MakeCode link https://makecode.com/_UDmAYyekRg9Y
+
+def clear(size):
+    # koordinātes norāda kastes diagonāles malas no kurai līdz kurai visus blokus aizvietot ar gaisu 
+    blocks.fill(AIR, pos(-1 * size, 0, -1 * size), pos(size, size, size), FillOperation.DESTROY)
+
+# izmantot ierakstot čatā "clear 5" vai "clear 15" utt
+player.on_chat("clear", clear)
